8148bccac4c1f94ac28904af2696bb6ca29b552a
Nimisha Mohapatra <51197219+Nimisha-97@users.noreply.github.com>
2019-08-14 15:55:03 +0530

    Add files via upload

---
+++ GamificationRulesBackend-master/GamificationRulesBackend-master/src/test/java/com/wipro/RulesGamification/RulesGamificationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wipro.RulesGamification;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RulesGamificationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
